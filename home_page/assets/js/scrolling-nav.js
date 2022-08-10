fa421405f66741d94dfeb0a95204d555fb87fad8
lidya <ernimauluddyiyah@gmail.com>
2022-08-10 15:34:24 +0700

    ubah header
    ngubah header, ngubah logo, ngubah slider

--- home_page/assets/js/scrolling-nav.js
+++ home_page/assets/js/scrolling-nav.js
@@ -1,4 +1,4 @@
-(function($) {
+mainNav(function($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
--- home_page/index.php
+++ home_page/index.php
@@ -25,7 +25,7 @@
     <link rel="stylesheet" type="text/css" href="assets/vendors/magnific-popup/magnific-popup.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/hovereffects.css"> 
     <!-- Theme Styles -->
-    <link href="assets/css/scrolling-nav.css" rel="stylesheet">
+    <link href="home_page/assets/css/scrolling-nav.css" rel="stylesheet">
 	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="assets/css/theme.css">
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
@@ -34,121 +34,9 @@
 
   <body>
     <!-- Header -->
-    <header id="top">
-      <!-- Navbar --> 
-	  <nav class="navbar navbar-expand-lg navbar-dark bg-white fixed-top" id="mainNav">
-		<div class="container">
-		  <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/smk/LogoSMK.png" alt="Logo SMK BM" width="150" height="140"><!-- Light<span>Up</span> --></a>  
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="ti-menu"></span>
-		  </button>
-		  <div class="collapse navbar-collapse" id="navbarResponsive">
-			<ul class="navbar-nav ml-auto">
-			<li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#top">Home</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#services">Services</a>
-			  </li>
-			   <li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#about">About</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#team">Team</a>
-			  </li>
-			   <li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#price">Price</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-			  </li>
-			</ul>
-		  </div>
-		</div>
-	  </nav>
-      <!-- End Navbar --> 
-		<section class="hero-large hero">
-			<div id="demo" class="carousel slide" data-ride="carousel">
-			  <ul class="carousel-indicators">
-				<li data-target="#demo" data-slide-to="0" class="active"></li>
-				<li data-target="#demo" data-slide-to="1"></li>
-				<li data-target="#demo" data-slide-to="2"></li>
-			  </ul>
-			  <div class="carousel-inner">
-				<div class="carousel-item active">
-				  <img src="assets/img/smk/c1.jpg" alt="smk" width="1100" height="500">
-				  <div class="container">
-				   <div class="carousel-caption">
-					<h2>Teknik Komputer Dan Jaringan</h2>
-					<p>SMKBM Pakisaji</p>
-				  </div> 
-				  </div>
-				
-				</div>
-				<div class="carousel-item">
-				  <img src="assets/img/smk/c2.jpg" alt="smk" width="1100" height="500">
-				    <div class="container">
-				  <div class="carousel-caption">
-					<h2>Desain Visual Komunikasi</h2>
-					<p>SMKBM Pakisaji</p>
-				  </div>   
-				</div>
-				</div>
-				<div class="carousel-item">
-				  <img src="assets/img/smk/c3.jpg" alt="smk" width="1100" height="500">
-				    <div class="container">
-				  <div class="carousel-caption">
-<<<<<<< HEAD:template home page/index.html
-					<h2>Sistem Informatika Jaringan Dan Aplikasi</h2>
-=======
-					<h2>Bisnis Daring</h2>
-					<p>SMKBM Pakisaji</p>
-				  </div>   
-				  </div>
-				</div>
-				<div class="carousel-item">
-				  <img src="assets/img/intro/banner-bg3.png" alt="New York" width="1100" height="500">
-				    <div class="container">
-				  <div class="carousel-caption">
-					<h2>Sistem Informatika Jaringan & Aplikasi</h2>
->>>>>>> main:home_page/index.html
-					<p>SMKBM Pakisaji</p>
-				  </div>   
-				  </div>
-				</div>
-			  </div>
-			  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-				<span class="carousel-control-prev-icon"></span>
-			  </a>
-			  <a class="carousel-control-next" href="#demo" data-slide="next">
-				<span class="carousel-control-next-icon"></span>
-			  </a>
-			</div>
-			<!-- <div class="hero-img bg-overlay" data-overlay="0" style="background-image: url(assets/img/intro/banner-bg.png);"></div> -->
-			<!-- <div class="container mt-md-5 pt-md-3"> -->
-				<!-- <div class="row text-white hero-content"> -->
-					<!-- <div class="col-md-6"> -->
-						<!-- <!-- heading -->  
-						<!-- <h1 class="text-uppercase mb-3 display-4 font-weight-bolder" data-aos="fade-right" data-aos-easing="linear"> -->
-							<!-- Change the World -->
-						<!-- </h1>  -->
-						<!-- <p class="lead text-white opacity-2" data-aos="fade-right" data-aos-easing="linear"> -->
-						<!-- Always deliver more than expected -->
-						<!-- </p> -->
-						<!-- <a class="btn btn-lg btn-outline-light py-2 mt-3 px-4 btn-pill" data-aos="fade-right" data-aos-easing="linear" src="#services" -->
-						<!-- href="#services">Our Services</a> -->
-					<!-- </div> -->
-					<!-- <div class="col-md-5"> -->
-					 <!-- <img src="assets/img/intro/rocket.svg" width="70%" class="float-right" alt="" data-aos="fade-left" data-aos-easing="linear" data-aos-delay="200"> -->
-					<!-- </div> -->
-				<!-- </div> -->
-			<!-- </div> -->
-		</section>
-
-    </header>
+   <?php 
+   include('segment/header.php');
+    ?>
     <!-- End Header -->
 
     <main role="main" class="mt-lg-5 m-0">
