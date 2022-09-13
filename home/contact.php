5a8ac3c499c64f813d75965157ae69d1f53951d7
Aninda <anindaindriani4@gmail.com>
2022-09-13 15:00:47 +0700

    page contact+header not fix

--- home/contact.php
+++ home/contact.php
@@ -2,32 +2,37 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+ <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Blog Single - BizPage Bootstrap Template</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <title>SMK Budi Mulia Pakisaji</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="img/smk/logo.png" rel="icon">
+    <link href="img/smk/logo.png" rel="apple-touch-icon">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- =======================================================
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style2.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <!-- =======================================================
   * Template Name: BizPage - v5.9.1
   * Template URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
   * Author: BootstrapMade.com
@@ -41,39 +46,67 @@
   <?php include 'layout/header.php';?>
 <!-- End Header -->
 
+  <!-- ======= hero Section ======= -->
+    <?php include 'index.php';?>
+    <!-- End Hero Section -->
+
+  <!-- ======= main ======= -->
   <main id="main">
 
     <!-- ======= Blog Single Section ======= -->
     <section id="blog" class="blog">
       <div class="container" data-aos="fade-up">
+        <div class="judl">
+          <h1>Kontak Kami</h1>
+          <div class="katakontak">
+            <p>Demi terjalinnya komunikasi yang baik sesama masyarakat dan SMK Budi Mulia Pakisaji, kami menyediakan wadah bagi Anda yang ingin bertanya ataupun memberikan saran kepada kami agar nantinya dapat menjadikan pihak SMK Budi Mulia Pakisaji lebih baik dalam melayani pendidikan.</p>
+          </div>
+        </div>
+        
+          <div class="blog-comments">
+            <div class="row">
+              <div class="col-8">col</div>
+              <div class="col-4">col</div>
+            </div>
+         
+        </div>
 
         <div class="row">
 
-          <div class="col-lg-8 entries">
+          <div class="col-lg entries">
 
             <div class="blog-comments">
                 <div class="reply-form">
                 <h4>Leave a Reply</h4>
                 <p>Your email address will not be published. Required fields are marked * </p>
                 <form action="">
-                  <div class="row">
-                    <div class="col-md-6 form-group">
-                      <input name="name" type="text" class="form-control" placeholder="Your Name*">
-                    </div>
-                    <div class="col-md-6 form-group">
-                      <input name="email" type="text" class="form-control" placeholder="Your Email*">
-                    </div>
+
+
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">Nama</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="nama" name="nama">
                   </div>
-                  <div class="row">
-                    <div class="col form-group">
-                      <input name="website" type="text" class="form-control" placeholder="Your Website">
-                    </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">E-mail</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="email" name="email">
                   </div>
-                  <div class="row">
-                    <div class="col form-group">
-                      <textarea name="comment" class="form-control" placeholder="Your Comment*"></textarea>
-                    </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">Subjek</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="Subjek" name="Subjek">
                   </div>
+                </div>
+                  <div class="row mb-3">
+                  <label for="inputPassword" class="col-sm-2 col-form-label">Pesan</label>
+                  <div class="col-sm-10">
+                    <textarea class="form-control" style="height: 150px" id="Subjek" name="Subjek"></textarea>
+                  </div>
+                </div>
+
                   <button type="submit" class="btn btn-primary">Post Comment</button>
 
                 </form>
@@ -90,28 +123,33 @@
       </div>
     </section><!-- End Blog Single Section -->
 
-  </main><!-- End #main -->
+  </main><!-- End main -->
 
   <!-- ======= Footer ======= -->
-
+<?php include('layout/footer.php');?>
 <!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-  <!-- Uncomment below i you want to use a preloader -->
-  <!-- <div id="preloader"></div> -->
+     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    <!-- Uncomment below i you want to use a preloader -->
+    <!-- <div id="preloader"></div> -->
+    <div id="preloader"></div>
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
 
 </body>
 
